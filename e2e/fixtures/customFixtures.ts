85e897e494969dad9527a8d92f16dba86eb2d087
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-02 01:14:28 +0530

    feat: add custom fixtures and implement registration validation tests

--- e2e/fixtures/customFixtures.ts
+++ e2e/fixtures/customFixtures.ts
@@ -1,13 +1,18 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from '../pages/auth/LoginPage';
+import { RegisterPage } from '../pages/auth/RegisterPage';
 
 type MyFixtures = {
   loginPage: LoginPage;
+  registerPage: RegisterPage;
 };
 
 export const test = base.extend<MyFixtures>({
   loginPage: async ({ page }, use) => {
     await use(new LoginPage(page));
+  },
+  registerPage: async ({ page }, use) => {
+    await use(new RegisterPage(page));
   },
 });
 
